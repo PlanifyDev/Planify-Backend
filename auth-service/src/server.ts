bc11967b80c710b7dcd886eb8e9d8d8789aae91f
mosta1489 <mosta148999@gmail.com>
2022-11-07 07:07:08 +0200

    use heroku database & edit confi of connection

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/userRouter";
 import { loggerMiddleware, errHandler, notFound } from "./middleware";
+import accessEnv from "./helpers/accessEnv";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-const port = 3000;
+const port = accessEnv("PORT") || 3000;
 
 app.use(loggerMiddleware);
 
