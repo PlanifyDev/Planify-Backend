cfa5a03eca8303e5224ccb1b96b1ed1e90a22aac
Mostafa Ahmed <mosta148999@gmail.com>
2023-02-05 06:40:23 +0200

    update project and version table

--- migrations/project.sql
+++ migrations/project.sql
@@ -3,13 +3,13 @@ CREATE SEQUENCE projects START WITH 1;
 CREATE TABLE project (
     id                    INTEGER PRIMARY KEY DEFAULT nextval('projects'),
     name                  VARCHAR(100) NOT NULL,
-    description           VARCHAR(250) NOT NULL,
     boundary              JSON NOT NULL,
-    door_position         JSON NOT NULL,
-    neighbors             JSON NOT NULL,
-    constraints           JSON ,
+    -- door_position         JSON NOT NULL,
+    -- neighbors             JSON NOT NULL,
+    -- constraints           JSON ,
     image_url             VARCHAR(150) NOT NULL,
     created_at            TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_trashed            BOOLEAN NOT NULL DEFAULT FALSE,
     user_id               VARCHAR(100) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
@@ -20,6 +20,8 @@ CREATE TABLE version (
     version_num              INTEGER NOT NULL,
     name                     VARCHAR(100) NOT NULL,
     image_url                VARCHAR(150) NOT NULL,
+    created_at               TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_trashed               BOOLEAN NOT NULL DEFAULT FALSE,
     project_id               INTEGER,
     FOREIGN KEY (project_id) REFERENCES project(id)
 );
