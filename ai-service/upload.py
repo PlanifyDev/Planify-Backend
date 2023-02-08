6f93535f2502c17fc6cd7c10666a05baf1a8156c
Mohamed Agour <mo.aggour@gmail.com>
2023-02-08 06:50:00 +0200

    Improved API and added icon support and some shadows

--- ai-service/upload.py
+++ ai-service/upload.py
@@ -8,15 +8,18 @@ from flask.cli import load_dotenv
 load_dotenv()
 access_key = os.environ.get('AWS_ACCESS_KEY_ID')
 secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-s3_url = os.environ.get('AWS_S3_URL')
-s3_reg = os.environ.get('AWS_REGION')
+s3_reg = os.environ.get('AWS_S3_REGION')
+bucket_name = os.environ.get('AWS_S3_BUCKET')
+
+s3_url = 'https://s3.' + s3_reg + '.amazonaws.com'
+
 
 s3 = boto3.client('s3',
                   aws_access_key_id=access_key,
                   aws_secret_access_key=secret_key)
 
 
-def upload_to_s3(mask, bucket_name, folder_name='projects'):
+def upload_to_s3(mask, folder_name='projects'):
 
     random_id = str(uuid.uuid4())
     mask_image = Image.fromarray(mask)
@@ -31,6 +34,7 @@ def upload_to_s3(mask, bucket_name, folder_name='projects'):
     res = s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_bytes, ContentType=content_type)
     status = res['ResponseMetadata']['HTTPStatusCode']
     if status == 200:
+        # print( s3_url + '/' + bucket_name + '/' + file_name)
         return s3_url + '/' + bucket_name + '/' + file_name
     else:
         return None
