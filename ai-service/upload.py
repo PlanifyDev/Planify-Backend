e9e252abb6e8ad0c7db13bb75f5872004db9461e
Mohamed Agour <mo.aggour@gmail.com>
2023-04-27 15:56:01 +0200

    Added upload to imgbb

--- ai-service/upload.py
+++ ai-service/upload.py
@@ -1,6 +1,9 @@
 import os
 import boto3
 import uuid
+
+import cv2
+import requests
 from PIL import Image
 import io
 from flask.cli import load_dotenv
@@ -13,14 +16,12 @@ bucket_name = os.environ.get('AWS_S3_BUCKET')
 
 s3_url = 'https://s3.' + s3_reg + '.amazonaws.com'
 
-
 s3 = boto3.client('s3',
                   aws_access_key_id=access_key,
                   aws_secret_access_key=secret_key)
 
 
 def upload_to_s3(mask, folder_name='projects'):
-
     random_id = str(uuid.uuid4())
     mask_image = Image.fromarray(mask)
 
@@ -38,3 +39,19 @@ def upload_to_s3(mask, folder_name='projects'):
         return s3_url + '/' + bucket_name + '/' + file_name
     else:
         return None
+
+
+def upload_to_imgbb(image):
+    _, encoded_image = cv2.imencode('.jpg', image)
+
+    response = requests.post(
+        "https://api.imgbb.com/1/upload",
+        data={
+            "image": encoded_image.tobytes(),
+        },
+        params={
+            "key": "8d493e029a3607ac3f4c520c14d9d2d1",
+        },
+    )
+    url = response.json()["data"]["url"]
+    print(url)
