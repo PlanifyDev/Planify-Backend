20d17f6ab720650f4983d41e07f37abcda16ceea
Mostafa Ahmed <79556633+mosta1489@users.noreply.github.com>
2023-05-02 20:25:44 +0300

    .

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -2,31 +2,31 @@
 
 touch .env
 
-echo SALT_ROUND=${SALT_ROUND} >> .env
-echo JWT_SECRET=${JWT_SECRET} >> .env
-echo VERIFY_EMAIL=${VERIFY_EMAIL} >> .env
-echo VERIFY_PASSWORD=${VERIFY_PASSWORD} >> .env
-echo DATABASE_URI_LOCAL=${DATABASE_URI_LOCAL} >> .env
-echo ENV_DB=${ENV_DB} >> .env
-echo ENV_CACHE=${ENV_CACHE} >> .env
-echo REDIS_URL_LOCAL=${REDIS_URL_LOCAL} >> .env
-echo DATABASE_URL_PROD=${DATABASE_URL_PROD} >> .env
-echo FRONT_END_URL=${FRONT_END_URL} >> .env 
+echo "SALT_ROUND=${SALT_ROUND}" >> .env
+echo "JWT_SECRET=${JWT_SECRET}" >> .env
+echo "VERIFY_EMAIL=${VERIFY_EMAIL}" >> .env
+echo "VERIFY_PASSWORD=${VERIFY_PASSWORD}" >> .env
+echo "DATABASE_URI_LOCAL=${DATABASE_URI_LOCAL}" >> .env
+echo "ENV_DB=${ENV_DB}" >> .env
+echo "ENV_CACHE=${ENV_CACHE}" >> .env
+echo "REDIS_URL_LOCAL=${REDIS_URL_LOCAL}" >> .env
+echo "DATABASE_URL_PROD=${DATABASE_URL_PROD}" >> .env
+echo "FRONT_END_URL=${FRONT_END_URL}" >> .env 
 
 # payment
-echo PAYPAL_ID=${PAYPAL_ID} >> .env 
-echo PAYPAL_SECRET=${PAYPAL_SECRET} >> .env
-echo RETURN_URL=${RETURN_URL} >> .env
-echo CANCEL_URL=${CANCEL_URL} >> .env
-echo AUTH_GRPC_URL=${AUTH_GRPC_URL} >> .env
+echo "PAYPAL_ID=${PAYPAL_ID}" >> .env 
+echo "PAYPAL_SECRET=${PAYPAL_SECRET}" >> .env
+echo "RETURN_URL=${RETURN_URL}" >> .env
+echo "CANCEL_URL=${CANCEL_URL}" >> .env
+echo "AUTH_GRPC_URL=${AUTH_GRPC_URL}" >> .env
 
 # design
-echo AI_GRPC_URL=${AI_GRPC_URL} >> .env
+echo "AI_GRPC_URL=${AI_GRPC_URL}" >> .env
 
 
 # log path
-echo AUTH_LOG_PATH=${ secrets.AUTH_LOG_PATH } >> .env 
-echo PAY_LOG_PATH=${ secrets.PAY_LOG_PATH } >> .env 
-echo DESIGN_LOG_PATH=${ secrets.DESIGN_LOG_PATH } >> .env 
+echo "AUTH_LOG_PATH=${ secrets.AUTH_LOG_PATH }" >> .env 
+echo "PAY_LOG_PATH=${ secrets.PAY_LOG_PATH }" >> .env 
+echo "DESIGN_LOG_PATH=${ secrets.DESIGN_LOG_PATH }" >> .env 
 
 
