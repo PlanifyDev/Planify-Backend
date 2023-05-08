13165117c8d1f33152ac9c13f215d2fbe0945a51
Mostafa Ahmed <mosta148999@gmail.com>
2023-05-09 00:55:50 +0300

    bullshit 3

--- payment-service/src/middleware/authByCache.ts
+++ payment-service/src/middleware/authByCache.ts
@@ -17,7 +17,7 @@ if (env === "prod") {
 
 export const authByCache = async (req, res, next) => {
   const token = req.headers.authorization;
-  const user_id = req.headers.user_id;
+  const user_id = req.headers.id;
 
   if (!token) {
     return res.status(401).send({ error: "Bad token" });
